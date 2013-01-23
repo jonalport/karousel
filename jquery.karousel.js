05fbf699650f88a89274d394b9fb056073876c96
jonalport <jon@jonalport.com>
2013-01-23 10:55:01 -0700

    JSHint:

--- jquery.karousel.js
+++ jquery.karousel.js
@@ -1,52 +1,48 @@
-;
 (function($) {
-/*
- * jquery.karousel.js - version 0.1
- * A simple jQuery carousel plugin
- * 
- * Copyright (c) 2013, Jon Alport (http://jonalport.com)
- * Licensed under the MIT License (MIT-LICENSE.txt)
- * http://www.opensource.org/licenses/mit-license.php
- * Created: 2013-01-22
- */
+  'use strict';
+  /*
+   * jquery.karousel.js - version 0.1
+   * A simple jQuery carousel plugin
+   * Copyright (c) 2013, Jon Alport (http://jonalport.com)
+   */
   $.fn.extend({          
     karousel: function(options) {
       // Default options
       var defaults = {
           firstSlide: 1
       };       
-      var options = $.extend(defaults, options);
+      options = $.extend(defaults, options);
       // For each matched element
-      return this.each(function(e) {
+      return this.each(function() {
         // Initialise
         moveToSlide(options.firstSlide);
         // Button handlers
-        $('#left').click(function(e) {
-          moveLeft();
+        $('#left').click(function() {
+          showSlideToLeft();
           return false;
         });
-        $('#right').click(function(e) {
-          moveRight();
+        $('#right').click(function() {
+          showSlideToRight();
           return false;
         });
         // Keypress handlers
         $(document).keydown(function(e) {
           // Left
-          if(e.keyCode == 37) { 
+          if(e.keyCode === 37) { 
             showSlideToLeft();
             return false;
           }
           // Right
-          else if(e.keyCode == 39) { 
+          else if(e.keyCode === 39) { 
             showSlideToRight();
             return false;
           }
         });
         // Move to slide number (slide numbers start at 1)
         function moveToSlide(slideNum) {
-          var slidePadding = parseInt($('#slides li').css('margin-left'));
-          var slideWidth = parseInt($('#slides li').css('width'));
-          var slideContainerWidth = parseInt($('#slides').parent().css('width'));
+          var slidePadding = parseInt($('#slides li').css('margin-left'), 10);
+          var slideWidth = parseInt($('#slides li').css('width'), 10);
+          var slideContainerWidth = parseInt($('#slides').parent().css('width'), 10);
           var totalSlideWidth = slideWidth + slidePadding * 2;
           var startPosition = totalSlideWidth * -1 * (slideNum - 1) + slideContainerWidth / 2 - slideWidth / 2 - slidePadding;
           $('ul#slides').css('margin-left', startPosition + 'px').children().removeClass('active').eq(slideNum - 1).addClass('active');
